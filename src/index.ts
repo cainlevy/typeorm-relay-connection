e0ed5b885786d532e93cf95c1f4e3d17cb7e3b7a
Lance Ivy <lance@cainlevy.net>
2022-11-09 15:01:54 -0800

    main exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export const hello = () => "hello world";
+export { default as RelayConnection, LimitError } from "./RelayConnection";
+export type { Args, Edge, Entity, PageInfo } from "./RelayConnection";
+export { default as TypeORMRelayConnection } from "./TypeORMRelayConnection";
